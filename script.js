292dc0bd639b4c9db8bf92d5f0ad5d59032c2935
manger-creator <84518421+manger-creator@users.noreply.github.com>
2021-06-11 02:00:46 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+$('#employee_table tbody tr').hide(
+    function(){
+        $(this).hide();
+    });
+
+
+      $(document).ready(function(){  
+           $('#search').keyup(function(){  
+                search_table($(this).val());  
+           });  
+           function search_table(value){  
+                $('#employee_table tbody tr').each(function(){  
+                     var found = 'false';  
+                     $(this).each(function(){  
+                          if($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0)  
+                          {  
+                               found = 'true';  
+                          }  
+                     });  
+                     if(found == 'true')  
+                     {  
+                          $(this).show();
+                     }  
+                     else  
+                     {  
+                          $(this).hide();  
+                     }  
+                });  
+           }  
+      });
+
+$('input:text').onclick(
+    function(){
+        $(this).val('');
+    });
